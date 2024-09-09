13695f0eff831a49301a2fe3a7708a29eeedacf3
Sachin <guitarplayersachin@gmail.com>
2024-09-09 13:53:07 +0530

    refixed 1_page

--- pages/1_idea_gen_ui.py
+++ pages/1_idea_gen_ui.py
@@ -11,6 +11,32 @@ import streamlit as st
 st.title("Idea Generator and Validator (v1)")
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 if "model" not in st.session_state:
     st.session_state.model = "gpt-4"
@@ -73,8 +99,7 @@ if prompt := st.chat_input("Enter your idea here..."):
     with st.chat_message("user"):
         st.markdown(prompt)
         st.session_state.chat_history.append({'role':'user', 'content':prompt})
-        #if("Solution" in st.session_state.chat_history[-1]):
-        #    print("hellllllooo",st.session_state.chat_history[-1])
+    
     with st.chat_message("assistant"):
         response = chain.predict(input=prompt, history=memory.chat_memory)
         st.session_state.chat_history.append({'role':'ai', 'content':response})
